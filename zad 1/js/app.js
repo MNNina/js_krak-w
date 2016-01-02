e1d0c72b960a42cda31b72f4f9b4ac651a11a7ed
Ninja <srebrzuch@gmail.com>
2016-01-02 15:59:15 +0000

    add word button shows alert on click

--- zad 1/js/app.js
+++ zad 1/js/app.js
@@ -2,6 +2,9 @@
  * Created by Ninja on 2016-01-02.
  */
 document.addEventListener("DOMContentLoaded", function(){
-
+    var addWordButton = document.querySelector("#addWordButton");
+    addWordButton.addEventListener("click", function(event){
+      alert("ok");
+  });
 
 });
